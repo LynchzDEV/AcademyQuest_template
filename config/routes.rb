f5bbde91e029e56460fd99c307c7570678283403
LynchzDEV <opmok48@gmail.com>
2025-05-14 20:25:07 +0700

    Add brag page and navigation to share 2025 goals

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :quests
   root "quests#index"
+  resources :quests
 
+  get "/brag", to: "brag#index", as: :brags
   get "up" => "rails/health#show", as: :rails_health_check
 end
